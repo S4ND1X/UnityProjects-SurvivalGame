527a3948606534bc16f7afc2c7a7f9f664d1f615
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-07-16 17:28:01 -0500

    15% Completed

---
+++ Survival Game/Assets/Scripts/colliderWea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class colliderWea : MonoBehaviour
+{  
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Si colisiona con el objeto con el nombre Wea
+        if(collision.gameObject.name =="Wea")
+        {
+            //Se destruye el objeto tocado.
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
